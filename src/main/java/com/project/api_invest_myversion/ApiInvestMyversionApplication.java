5572f690ecdd9bc34ac133704be227b534875896
bragancaDev9 <bryanstos99@gmail.com>
2026-02-27 18:04:20 -0300

    First commit

---
+++ src/main/java/com/project/api_invest_myversion/ApiInvestMyversionApplication.java
@@ -0,0 +1,13 @@
+package com.project.api_invest_myversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInvestMyversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInvestMyversionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/api_invest_myversion/ApiInvestMyversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api_invest_myversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInvestMyversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
